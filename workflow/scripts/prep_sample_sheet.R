207e359e6dc26d28bfbf79b5b5d7721ecfd45550
lichtobergo <62296530+lichtobergo@users.noreply.github.com>
2024-05-29 14:14:49 +0200

    Pipeline configures

--- workflow/scripts/prep_sample_sheet.R
+++ workflow/scripts/prep_sample_sheet.R
@@ -2,62 +2,21 @@ library(tidyverse)
 library(readxl)
 
 sample_sheet <- read_delim(
-  "config/sample_sheet.txt", 
+  "config/metadata.tsv", 
   delim = "\t", escape_double = FALSE, 
-  col_types = cols(Comments = col_skip()), 
+  col_types = cols(µl.of.RNA = col_skip(),
+                   files = col_skip()), 
   trim_ws = TRUE
 ) %>% 
-  dplyr::filter(
-    sample.name != "N_04"
-  ) %>% 
-  relocate(
-    sample.name,
-    .before = sample.number
-  ) %>% 
-  arrange(sample.name) %>% 
-  mutate(
-    sample.name = str_remove(sample.name, "_")
+  rename(
+    sample.name = names
   )
-# %>% 
-#   dplyr::select(1:6) %>% 
-#   rename(animal.number = "...6")
-# colnames(sample_sheet_p1425) <- str_replace_all(colnames(sample_sheet_p1425), " ", ".")
-# sample_sheet_p1425 <- sample_sheet_p1425 %>% 
-#   mutate_all(
-#     ~ str_replace(., " ", "_")
-#     ) %>% 
-#   mutate(
-#     tissue = str_split_i(sample.group, "_", i = 2),
-#     tissue = if_else(
-#       tissue == "spinal cord",
-#       "SC",
-#       tissue
-#     ),
-#     sample.group = str_split_i(sample.group, "_", i = 1),
-#     group = case_when(
-#       str_detect(sample.group, "CY") ~ "TovaCAR.+AAV",
-#       str_detect(sample.group, "TY") ~ "Tova.+AAV",
-#       str_detect(sample.group, "CN") ~ "TovaCAR.noAAV",
-#       str_detect(sample.group, "TN") ~ "Tova.noAAV"
-#     )
-#   )
 
 units <- tibble(
   sample.name = sample_sheet$sample.name,
-  read1 = list.files(path = "data/fastq", pattern = "*_R1", recursive = T, full.names = TRUE),
-  read2 = list.files(path = "data/fastq", pattern = "*_R2", recursive = T, full.names = TRUE)
+  read1 = list.files(path = "data/fastq", pattern = "*_R1", full.names = TRUE),
+  read2 = list.files(path = "data/fastq", pattern = "*_R2", full.names = TRUE)
 )
-
-# remove sample H_04 because it is likely an outlier based on pca and fastQC
-units <- units %>% 
-  dplyr::filter(
-    sample.name != "H04"
-  )
-sample_sheet <- sample_sheet %>% 
-  dplyr::filter(
-    sample.name != "H04"
-  )
-
 
 write_delim(sample_sheet,
           file = "config/sample_sheet.tsv",
